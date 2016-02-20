c94b3bcd26268f9644e9bf3e32455d8f7efad7d4
liming <li_enterprise@163.com>
2016-02-20 20:20:20 +0800

    rewrite initiator.py

--- initiator.py
+++ initiator.py
@@ -22,47 +22,54 @@ def crawl(spiders, query, start, end, mode):
     process.start()
 
 if __name__ == "__main__":
-    # 时间
-    start_time = None
-    gzh_order = None
-    crawl_mode = None
-    if "-s" in sys.argv: # 传入start_time
-        idx = sys.argv.index("-s")
-        start_time = datetime.strptime(sys.argv[idx+1], "%Y/%m/%d/%H:%M:%S")
-    if "-c" in sys.argv:  # 从数据库读取距现在最近的爬取时间作为启动时间
-        # 此选项用于宕机重启，目前的做法不能保证宕机前最后一次爬取完成，会漏掉一部分新闻
-        conn = MySQLdb.connect(**DB_CONFIG)
-        cur = conn.cursor()
-        cur.execute("select max(create_time) from media_info")
-        start_time = cur.fetchone()[0]
-        cur.close()
-    if "-o" in sys.argv: # 按照各个公众号已爬文章数目递增的顺序来确定公众号的爬取顺序，确保已爬文章少的公众号能被优先爬取
-        # 此选项仅会爬取在数据库中有文章记录数的公众号，所以新加入的公众号需要爬取历史数据后才能被该选项爬取到
-        conn = MySQLdb.connect(**DB_CONFIG)
-        cur = conn.cursor()
-        cur.execute("select a.name from media as a right join media_info as b on a.id=b.media_id group by b.media_id order by count(1)")
-        result = cur.fetchall()
-        gzh_order = [x[0] for x in result]
-        cur.close()
+    # 静态参数设置
     if "-m" in sys.argv: # 爬取模式
         idx = sys.argv.index("-m")
-        crawl_mode =sys.argv[idx+1]
-
+        crawl_mode = int(sys.argv[idx+1])
+    else:
+        crawl_mode = 1 # 默认全量抓取
     while True:
-        end_time = datetime.now()  # 默认截止时间：爬虫启动时
-        if not start_time:
-            start_time = datetime.now()-timedelta(days=100) # 默认开始时间，100天以前
-        if not gzh_order:
+        # 动态设置公众号爬取顺序
+        if "-o" in sys.argv: # 按照各个公众号已爬文章数目递增的顺序来确定公众号的爬取顺序，确保已爬文章少的公众号能被优先爬取
+            # 此选项仅会爬取在数据库中有文章记录数的公众号，所以新加入的公众号需先爬取历史数据后才能被该选项爬取到
             conn = MySQLdb.connect(**DB_CONFIG)
             cur = conn.cursor()
-            cur.execute("select name from media")
+            cur.execute("""select media_id from media_info group by media_id order by count(1)""")
+            # 以下版本使用公众号的名字作为参数传递，后来发现有点繁琐，改用公众号的序号
+            # cur.execute("""select a.name from media as a right join media_info as b on a.id=b.media_id group by b.media_id order by count(1)""")
             result = cur.fetchall()
             gzh_order = [x[0] for x in result]
-            random.shuffle(gzh_order) # 默认随机排列待抓取公众号
             cur.close()
-        if not crawl_mode:
-            crawl_mode = 1 # 默认全量抓取
-        for query in gzh_order:
+        else: # 默认随机爬取所有的公众号
+            conn = MySQLdb.connect(**DB_CONFIG)
+            cur = conn.cursor()
+            cur.execute("""select id from media""")
+            result = cur.fetchall()
+            gzh_order = [x[0] for x in result]
+            random.shuffle(gzh_order) # 打乱公众号排列顺序来随机爬取
+            cur.close()
+        for gzh_id in gzh_order:
+            # 设置各个公众号的爬取起止时间
+            end_time = datetime.now()  # 默认截止时间：爬虫启动时
+            if "-s" in sys.argv: # 传入start_time
+                idx = sys.argv.index("-s")
+                start_time = datetime.strptime(sys.argv[idx+1], "%Y/%m/%d/%H:%M:%S")
+            elif "-c" in sys.argv:  # 从数据库读取待爬公众号距现在最近的文章发布时间的之前一天作为启动时间，达到增量更新的效果
+                # 此选项适用于每轮爬取周期较短时，每个公众号积累的文章数较少，可以一次爬完。若积累文章数较多，则爬取时可能被反爬，会漏掉一些文章
+                # 目前不推荐使用，所以才用爬取模式crawl_mode来替代它的效果
+                conn = MySQLdb.connect(**DB_CONFIG)
+                cur = conn.cursor()
+                cur.execute("""select max(publish_time) from media_info where media_id=%s""",(unicode(gzh_id),))
+                lasttime = cur.fetchone()[0]
+                start_time = lasttime-timedelta(days=1) # 由于数据库里文章的发布时间均是0点，为防止上次爬取当天更新的文章被忽略，需把起始时间提前一天
+                cur.close()
+            else:
+                start_time = end_time-timedelta(days=365) # 默认开始时间，一年之前
+            conn = MySQLdb.connect(**DB_CONFIG)
+            cur = conn.cursor()
+            cur.execute("""select name from media where id=%s""",(unicode(gzh_id),))
+            query = cur.fetchone()[0]
+            cur.close()
             pool = Pool(processes=1) # 进程池，由于公众号的反爬限制较严，限制为单进程
             pool.apply(crawl, args=('wechat', query, start_time, end_time, crawl_mode))
             pool.close()
